5f84bef3310b23d02b300d6c938d7634dc3784d2
masakiyo okuhama <e165725@ie.u-ryukyu.ac.jp>
2016-12-13 11:33:53 +0900

    中身はない

---
+++ src/main/jp.ac.uryukyu.ie.e165725/ExMain.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e165725;
+
+/**
+ * Created by okuhamamasakiyo on 2016/12/06.
+ */
+
+public class ExMain{
+    public static void main(String[] arg){
+        ExMain insetance = new ExMain();
+    }
+    String map(String[] arg){
+
+    }
+    String player(String[] arg){
+        
+    }
+}
+
+
